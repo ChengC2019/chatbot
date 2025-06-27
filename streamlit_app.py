a0c040807531627ac6b6f1bffc3fbec6bd0bd86c
Cheng Chen <cheng.chen@uah.edu>
2025-06-27 19:14:09 +0000

    Updates

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,18 +1,23 @@
 import streamlit as st
 from openai import OpenAI
+from dotenv import load_dotenv
+import os
 
 # Show title and description.
-st.title("💬 Chatbot")
+st.title("💬 Teaching Assistant")
 st.write(
     "This is a simple chatbot that uses OpenAI's GPT-3.5 model to generate responses. "
     "To use this app, you need to provide an OpenAI API key, which you can get [here](https://platform.openai.com/account/api-keys). "
     "You can also learn how to build this app step by step by [following our tutorial](https://docs.streamlit.io/develop/tutorials/llms/build-conversational-apps)."
 )
+load_dotenv()
 
+USER_AVATAR = "👤"
+BOT_AVATAR = "🤖"
 # Ask user for their OpenAI API key via `st.text_input`.
 # Alternatively, you can store the API key in `./.streamlit/secrets.toml` and access it
 # via `st.secrets`, see https://docs.streamlit.io/develop/concepts/connections/secrets-management
-openai_api_key = st.text_input("OpenAI API Key", type="password")
+openai_api_key = st.secrets["openai"]["api_key"]
 if not openai_api_key:
     st.info("Please add your OpenAI API key to continue.", icon="🗝️")
 else:
